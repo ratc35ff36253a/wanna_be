1bdf352f48f95102db542ac0922ab83f2515bf68
Franc6220 <samuelwangalibo@yahoo.com>
2024-08-27 16:24:26 +0300

    landing-page

---
+++ marketing/my-app/components/navbar.tsx
@@ -0,0 +1,80 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+
+    const isAdmin = user?.email === process.env.ADMIN_EMAIL
+    
+    return ( 
+        <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                    <Link href='/' className="flex z-40 font-semibold">
+                    joy's<span className="text-blue-600">enterprise</span>
+                    </Link>
+
+                    <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent
+                    bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+                        {user ? (
+                            <>
+                            <Link href='/api/auth/logout' className={buttonVariants({
+                                size: 'sm',
+                                variant: 'ghost',
+                            })}>
+                                Sign out
+                            </Link>
+                            {isAdmin ? (
+                                <Link href='/dashboard' className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost',
+                                })}>
+                                    Dashboard
+                                </Link>
+                            ) : null}
+                            <Link href='/api/auth/login' className={buttonVariants({
+                                size: 'sm',
+                                className: 'hidden sm:flex items-center gap-1',
+                            })}>
+                                Book Appointment
+                                <ArrowRight className='ml-1.5 h-5 w-5' />
+                            </Link>
+                            </>
+                        ) : (
+                            <>
+                            <Link href='/api/auth/register' className={buttonVariants({
+                                size: 'sm',
+                                variant: 'ghost',
+                            })}>
+                                Sign up
+                            </Link>
+                            <Link href='/api/auth/login' className={buttonVariants({
+                                size: 'sm',
+                                variant: 'ghost',
+                            })}>
+                                Log in
+                            </Link>
+
+                            <div className='h-8 w-px bg-zinc-200 hidden sm:block'/>
+
+                            <Link href='/api/auth/login' className={buttonVariants({
+                                size: 'sm',
+                                className: 'hidden sm:flex items-center gap-1',
+                            })}>
+                                Book Appointment
+                                <ArrowRight className='ml-1.5 h-5 w-5' />
+                            </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+                </MaxWidthWrapper>
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ marketing/my-app/src/app/page.tsx
@@ -0,0 +1,56 @@
+import Navbar from "@/components/navbar";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+import { Icon, Star } from "lucide-react";
+import { Icons } from "@/components/Icons";
+
+export default function Home() {
+  return (
+    <div className='bg-slate-50'>
+      <section>
+        <MaxWidthWrapper className="bg-gradient-to-r from-green-500 to-green-300 text-white h-screen flex flex-col justify-center items-center px-4">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center">
+        Welcome to Joy's Enterprise the only plac where your Business can be on the Frontline of Everything
+        </h1>
+        <p className="text-xl md:text-2xl mb-8 text-center">
+          We help you market your business with ease and confidence
+        </p>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className='bg-slate-100 py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+              What our <span className='relative px-2'>customers <Icons.Underline className='hidden sm:block pointer-events-none absolute inset-x-0
+              -bottom-6 text-green-500'/></span>{''} say
+            </h2>
+            <img src="/phone.jpg" className='w-24 order-0 lg:order-2' />
+          </div>
+
+          <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-orange-500 fill-orange-400' />
+                <Star className='h-5 w-5 text-orange-500 fill-orange-400' />
+                <Star className='h-5 w-5 text-orange-500 fill-orange-400' />
+                <Star className='h-5 w-5 text-orange-500 fill-orange-400' />
+                <Star className='h-5 w-5 text-orange-500 fill-orange-400' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "At first I was not sure nor did I place trust on this company because of the favourable prices being offered but with time My client grew 
+                  and my product is being supplied not only within but also neighbouring counties.<span className='p-0.5 bg-slate-800 text-white'>My business profits have grown tremendously.</span>, all thanks to JOY'S ENTERPRISE"
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img className='rounded-full h-12 w-12 object-cover' src="/users/user-1"/>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
